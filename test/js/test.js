152d667b28340f58baaaa6598c112143a6189015
qgj210213 <qgj210213@163.com>
2019-01-20 18:58:08 +0900

    test.js

--- test/js/test.js
+++ test/js/test.js
@@ -4,4 +4,41 @@ function dotest(){
     console.log("qgj"+x);
 
 }
-dotest();
+/** 文字列を切り取り 処理*/
+function  doTextCut(){
+    var strContent = "abcdefghijklmnopqrstuvwxyz";
+    var strlen = strContent.length;
+    var loopCnt =0; //繰り返し数
+    if (strlen%10==0){
+        loopCnt = strlen/10;
+    }else{
+        loopCnt = parseInt(strlen/10) +1 ;
+    }
+    console.log("length:"+strlen+"--loop cunt"+loopCnt);
+    // 十桁数ずつ、配列に格納します。
+    var textArry = new Array();
+    for (var i = 0; i < loopCnt; i++) {
+        textArry[i] = strContent.substring(0,10);
+        strContent = strContent.substring(10,strContent.length);
+        console.log("array:--"+i+"--content:--"+textArry[i]+"--strContent:--"+strContent);
+    }
+}
+/** 文字列を切り取り 処理*/
+function  doTextCut2(){
+    var strContent = "abcdefghijklmnopqrstuvwxyz";
+    var textArry = new Array();
+    var cunt = 0;
+    do{
+        textArry[cunt] = strContent.substring(0,10);
+        strContent = strContent.substring(10,strContent.length);
+        // console.log("array:--"+cunt+"--content:--"+textArry[cunt]+"--strContent:--"+strContent);
+        cunt++;
+    }while(strContent.length > 10)
+    console.log("cunts:"+cunt);
+    textArry[cunt] = strContent;
+    textArry.forEach(function(item,index,textArry){
+        console.log("item--"+item+"--index--"+index);
+    })   
+}
+// doTextCut();
+doTextCut2();
--- test/js/txtFileRead.js
+++ test/js/txtFileRead.js
@@ -12,6 +12,24 @@ function readTxTfile(){
     // file path
     // var path = "F:\WorkSpaceDemo\WEBSpace\typeWeb\file\English\myName.txt";
     var path = "../../file/English/myName.txt";
-    $("#textContent").load(path);
-    console.log("ok");
+    var text ="";
+    $("#textContent").load(path,function(responseTxt,statusTxt,xhr){
+        if(statusTxt=="success")
+          alert("外部内容加载成功!");
+          // 获取div标签的内容
+          text = responseTxt
+          console.log("qgj:"+text);
+        // 追加标签处理
+        doTagAdd(text);
+        if(statusTxt=="error")
+          alert("Error: "+xhr.status+": "+xhr.statusText);
+      }); 
+}
+/** 追加标签处理 */
+function doTagAdd(textContent){
+    alert("show content!"+textContent);
+    var strLen = textContent.length;
+    alert(strLen);
+    
+
 }
